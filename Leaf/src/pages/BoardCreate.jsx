0b51a9d5ec7283194a8c1a29a047adc59a800c24
jeongenjin <jej12171010s@gmail.com>
2025-01-12 23:16:12 +0900

    게시판 작성 만드는 중

--- Leaf/src/pages/BoardCreate.jsx
+++ Leaf/src/pages/BoardCreate.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 function BoardCreate(){
+
+    const [boardContent, setBoardContent] = useState ({
+        title:'',
+        content: ''
+    })
+
+    const getValue = (e) => {
+        const {name, value} = e.target;
+        setBoardContent ({
+            ...boardContent,
+            [name]:value
+        })
+        console.log(boardContent);
+    }
+
+    const [viewContent, setViewContent] = useState([]);
+
+    const sumit = () => {
+        setViewContent(viewContent.concat({...boardContent}));
+        console.log(viewContent);
+    }
+
     return (
-        <div>
-            <input></input><br></br>
-            <input></input>
-            <button>게시하기</button>
+        <div className="BoardCreate">
+            <div className="addListBox">
+                <input placeholder="제목" onChange={getValue} name="title"/><br></br>
+                <input placeholder="내용" onChange={getValue} name="content"/>
+                <button onClick={sumit}>게시하기</button>
+            </div>
+
+        {viewContent.map((i,index)=> {
+            return (
+                <div className="listBox">
+                    <p key={index}>1</p>
+                    <p>2025.1.12</p>
+                    <p>{i.title}</p>
+                    <p>{i.content}</p>
+                    <button>수정하기</button>
+                    <button>삭제하기</button>
+                </div>
+            )
+        })}
+            
         </div>
     );
 }
